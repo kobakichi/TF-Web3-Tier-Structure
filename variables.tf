10530ca4a573b913a7878e02cb155996bc510796
kobakichi <fenderstrat57vin@gmail.com>
2023-06-29 16:25:54 +0900

    add vpc.tf created

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "vpc cidrblock"
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
---
+++ vpc.tf
@@ -0,0 +1,37 @@
+# VPC
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
+}
+
+# Public Subnets
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  availability_zone = var.availability_zones[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
+
+  tags = {
+    Name = "${var.prefix}-public-${var.availability_zones[count.index]}"
+  }
+}
+
+# Private Subnets
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  availability_zone = var.availability_zones[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 11)
+
+  tags = {
+    Name = "${var.prefix}-private-${var.availability_zones[count.index]}"
+  }
+}
